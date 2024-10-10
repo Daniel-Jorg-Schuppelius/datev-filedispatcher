a23238c3b3d89c79e1c2fbdd9d090166e8119a23
Daniel Jörg Schuppelius <schuppeliusd@schuppelius-gierke.org>
2024-10-10 17:51:34 +0200

    mapping for internalstore

--- src/Services/Payroll/PayrollFileService.php
+++ src/Services/Payroll/PayrollFileService.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 
 namespace App\Services\Payroll;
 
+use App\Config\Config;
 use App\Contracts\Abstracts\FileServiceAbstract;
 use Psr\Log\LoggerInterface;
 
@@ -28,8 +29,13 @@ class PayrollFileService extends FileServiceAbstract {
         return '/^(\d{5})_(\d{5})_([A-Za-z]+_[A-Za-z]+)_(\d{2})_(\d{4})_Brutto_Netto_([A-Z0-9]{2,3})\.pdf$/';
     }
 
+    public static function getDestinationFolder(): string {
+        $config = Config::getInstance();
+        return $config->internalStorePath . 'payroll';
+    }
+
     public function process(): void {
-        $this->logger->info("Verabreite Payroll Datei: $this->filename");
+        $this->logger->info("Verarbeite Payroll Datei: $this->filename");
         $this->extractDatafromFilename();
     }
 
