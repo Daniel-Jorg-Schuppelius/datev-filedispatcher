4b91cab3d16f355f54776f46ace01d20378458b8
DSchuppelius <daniel.schuppelius@googlemail.com>
2024-10-18 12:31:06 +0200

    add more loggingoptions

--- src/Services/Payroll/PayrollFileService.php
+++ src/Services/Payroll/PayrollFileService.php
@@ -13,18 +13,12 @@ declare(strict_types=1);
 namespace App\Services\Payroll;
 
 use App\Contracts\Abstracts\PeriodicFileServiceAbstract;
-use App\Helper\FileSystem\File;
 
 class PayrollFileService extends PeriodicFileServiceAbstract {
     // 00000_00000_Client_Client_00_0000_Brutto_Netto_AA0.pdf
     //                               1       2            3                4       5                          6
     protected const PATTERN = '/^(\d{5})_(\d{5})_([A-Za-z]+_[A-Za-z]+)_(\d{2})_(\d{4})_Brutto_Netto_([A-Z0-9]{2,3})\.pdf$/';
     protected const SUBFOLDER = "02 Entgeltabrechnung";
-
-    public function process(): void {
-        $this->logger->info("Verarbeite Payroll Datei: {$this->filename}");
-        File::move($this->filename, $this->getDestinationFolder());
-    }
 
     protected function extractDataFromFilename(): void {
         $matches = $this->getMatches();
