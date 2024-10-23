2942c3142a5b78d68cb68eb50899cc8dbaf02bcc
DSchuppelius <daniel.schuppelius@googlemail.com>
2024-10-23 13:28:58 +0200

    change timecode

---
+++ tests/PreProcessServices/TimeCodeProcessFileServiceTest.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * Created on   : Tue Oct 08 2024
+ * Author       : Daniel Jörg Schuppelius
+ * Author Uri   : https://schuppelius.org
+ * Filename     : PayrollFileServiceTest.php
+ * License      : MIT License
+ * License Uri  : https://opensource.org/license/mit
+ */
+
+namespace Tests\PreProcessServices;
+
+use App\Helper\FileDispatcher;
+use App\PreProcessServices\TimeCodeProcessFileService;
+use Tests\Endpoints\DocumentManagement\DocumentTest;
+
+class TimeCodeProcessFileServiceTest extends DocumentTest {
+    public function __construct($name) {
+        parent::__construct($name);
+        // Pfad zur Testdatei
+        // $this->testFile = realpath(__DIR__ . '/../../.samples/20542_10_2024_Brutto_Netto - 20241021132856_00001_AA0.pdf');
+        $this->testFile = realpath(__DIR__ . '/../../.samples/20542_10_2024_Brutto_Netto - 20241021_132856_00001_AA0.pdf');
+        $this->apiDisabled = true; // API is disabled
+    }
+
+    public function testPatternMatching() {
+        if (empty($this->testFile)) {
+            $this->markTestSkipped('Test file not found');
+        }
+
+        $matches = [];
+        $this->assertTrue(TimeCodeProcessFileService::matchesPattern($this->testFile, $matches));
+        $this->assertIsArray($matches);
+        $this->assertCount(2, $matches);
+    }
+
+    public function testMatchesPattern(): void {
+        if (empty($this->testFile)) {
+            $this->markTestSkipped('Test file not found');
+        }
+
+        $this->assertTrue(TimeCodeProcessFileService::matchesPattern($this->testFile));
+
+        $invalidFilename = 'some_invalid_file_name.txt';
+        $this->assertFalse(TimeCodeProcessFileService::matchesPattern($invalidFilename));
+    }
+
+    public function testPayrollFileServiceProcessing() {
+        if ($this->apiDisabled) {
+            $this->markTestSkipped('API is disabled');
+        } elseif (empty($this->testFile)) {
+            $this->markTestSkipped('Test file not found');
+        }
+
+        $service = new TimeCodeProcessFileService($this->testFile);
+        $service->preProcess();
+
+        $this->assertEquals('20542', $service->getClient()->getNumber());
+    }
+
+    public function testFileDispatcherPayrollFileServiceProcessing(): void {
+        if ($this->apiDisabled) {
+            $this->markTestSkipped('API is disabled');
+        } elseif (empty($this->testFile)) {
+            $this->markTestSkipped('Test file not found');
+        }
+
+        $this->assertFileExists($this->testFile);
+
+        FileDispatcher::processFile($this->testFile);
+    }
+}
