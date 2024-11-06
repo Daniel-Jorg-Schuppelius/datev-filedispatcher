114eef73a8eb0d4bc94d7e4525df6f3274bcd620
DSchuppelius <daniel.schuppelius@gmail.com>
2024-11-06 19:47:02 +0100

    fix personnelreport pattern

--- src/Services/Payroll/PaymentTransferProtocolFileService.php
+++ src/Services/Payroll/PaymentTransferProtocolFileService.php
@@ -16,7 +16,7 @@ use App\Contracts\Abstracts\FileServices\Periodic\PayrollFileServiceAbstract;
 
 class PaymentTransferProtocolFileService extends PayrollFileServiceAbstract {
     // 00000_10_2024_Überweisungsprotokoll_AA0.pdf
-    //                                        1               2              3                                 4
+    //                                        1               2              3                                   4
     protected const PATTERN = '/^(?<tenant>\d{5})_(?<month>\d{2})_(?<year>\d{4})_Überweisungsprotokoll_([A-Z0-9]{2,3})\.pdf$/i';
 
     protected function getDestinationFilename(): string {
--- src/Services/Payroll/PersonnelReportFileService.php
+++ src/Services/Payroll/PersonnelReportFileService.php
@@ -12,16 +12,20 @@ declare(strict_types=1);
 
 namespace App\Services\Payroll;
 
+use APIToolkit\Enums\Month;
 use App\Contracts\Abstracts\FileServices\Periodic\PayrollFileServiceAbstract;
 
 class PersonnelReportFileService extends PayrollFileServiceAbstract {
     // 00000_10_2024_Personalreport_AA0.pdf
-    //                                        1               2              3                                 4
-    protected const PATTERN = '/^(?<tenant>\d{5})_(?<month>\d{2})_(?<year>\d{4})_Personalreport_([A-Z0-9]{2,3})\.pdf$/i';
+    //                                        1               2              3                                    4                    5             6
+    protected const PATTERN = '/^(?<tenant>\d{5})_(?<month>\d{2})_(?<year>\d{4})_Personalreport_(?<reportmonth>\d{2})_(?<reportyear>\d{4})_([A-Z0-9]{2,3})\.pdf$/i';
 
     protected function getDestinationFilename(): string {
+        $matches = $this->getMatches();
         $documentType = "Personalreport";
 
-        return "{$documentType}.pdf";
+        $namedMonth = Month::toArray(false, 'de')[(int) $matches["reportmonth"]];
+
+        return "{$documentType} - {$namedMonth} {$matches["reportyear"]}.pdf";
     }
 }
