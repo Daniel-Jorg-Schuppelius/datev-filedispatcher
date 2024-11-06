bfc4d0ecd8fc53826c33aa320e57682b2ff74171
DSchuppelius <daniel.schuppelius@gmail.com>
2024-11-06 19:28:12 +0100

    Add to Payrollfileservices

---
+++ src/Services/Payroll/PaymentTransferProtocolFileService.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Created on   : Sat Oct 19 2024
+ * Author       : Daniel Jörg Schuppelius
+ * Author Uri   : https://schuppelius.org
+ * Filename     : PaymentTransferProtocolFileService.php
+ * License      : MIT License
+ * License Uri  : https://opensource.org/license/mit
+ */
+
+declare(strict_types=1);
+
+namespace App\Services\Payroll;
+
+use App\Contracts\Abstracts\FileServices\Periodic\PayrollFileServiceAbstract;
+
+class PaymentTransferProtocolFileService extends PayrollFileServiceAbstract {
+    // 00000_10_2024_Überweisungsprotokoll_AA0.pdf
+    //                                        1               2              3                                 4
+    protected const PATTERN = '/^(?<tenant>\d{5})_(?<month>\d{2})_(?<year>\d{4})_Überweisungsprotokoll_([A-Z0-9]{2,3})\.pdf$/i';
+
+    protected function getDestinationFilename(): string {
+        $documentType = "Überweisungsprotokoll";
+
+        return "{$documentType}.pdf";
+    }
+}
