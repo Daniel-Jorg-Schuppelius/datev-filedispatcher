9d166591facfd156ec5b5acaf29ba6412d2992b4
DSchuppelius <daniel.schuppelius@gmail.com>
2025-01-03 14:34:41 +0100

    mehrmonatige bezüge

---
+++ src/Services/Payroll/SettlementOfGrossNetRemunerations4SeveralMonthsFileService.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Created on   : Fri Jan 03 2025
+ * Author       : Daniel Jörg Schuppelius
+ * Author Uri   : https://schuppelius.org
+ * Filename     : SettlementOfGrossNetRemunerations4SeveralMonthsFileService.php
+ * License      : MIT License
+ * License Uri  : https://opensource.org/license/mit
+ */
+
+declare(strict_types=1);
+
+namespace App\Services\Payroll;
+
+use App\Contracts\Abstracts\FileServices\Periodic\PayrollFileServiceAbstract;
+use App\Helper\InternalStoreMapper;
+use Exception;
+
+class SettlementOfGrossNetRemunerations4SeveralMonthsFileService extends PayrollFileServiceAbstract {
+    // 00000_10_2024_10_2024_Brutto_Netto_00001.pdf
+    //                                        1       2       3               4              5                    6
+    protected const PATTERN = '/^(?<tenant>\d{5})_(\d{2})_(\d{4})_(?<month>\d{2})_(?<year>\d{4})_Brutto_Netto_(\d{5})\.pdf$/i';
+
+    protected function getDestinationFilename(): string {
+        $matches = $this->getMatches();
+
+        $documentType = "Mehrmonatige Entgeltabrechnung";
+
+        if (!is_null($this->payrollClient)) {
+            $this->logInfo("Client gefunden: {$this->payrollClient->getNumber()}");
+
+            $employees = $this->payrollClient->getEmployees();
+            $employee = $employees->getFirstValue('id', $matches[5]);
+            if (!is_null($employee)) {
+                $this->logInfo('Mitarbeiter gefunden: ' . $employee->getSurname() . ' ' . $employee->getFirstName());
+                return "{$documentType}_{$matches[6]}_{$employee->getSurname()}_{$employee->getFirstName()}.pdf";
+            }
+
+            $this->logError("Mitarbeiter nicht gefunden: {$matches[6]}");
+            throw new Exception("Mitarbeiter nicht gefunden: {$matches[6]}");
+        }
+
+        $this->logError("Client nicht gefunden: {$matches[1]}");
+        throw new Exception("Client nicht gefunden: {$matches[1]}");
+    }
+
+    public function getDestinationFolder(bool $leadingZero = true): ?string {
+        $subFolder = $this->getSubFolder();
+
+        [$yearFormatted, $monthFormatted] = $this->getFormattedDateParts($leadingZero);
+
+        $subFolder = $this->prepareSubFolder($subFolder, false, true);
+
+        $this->logger->info("Nutze Jahresablage für den Ordner '" . $subFolder . "'.");
+        return InternalStoreMapper::getInternalStorePath($this->client, $subFolder, $yearFormatted);
+    }
+}
