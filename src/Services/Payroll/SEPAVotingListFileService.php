ea86bb201082faef74350cfb1fc642d679a8e3df
DSchuppelius <daniel.schuppelius@googlemail.com>
2025-08-27 14:12:03 +0200

    update

--- src/Services/Payroll/SEPAVotingListFileService.php
+++ src/Services/Payroll/SEPAVotingListFileService.php
@@ -13,11 +13,10 @@ declare(strict_types=1);
 namespace App\Services\Payroll;
 
 use App\Contracts\Abstracts\FileServices\Periodic\PayrollFileServiceAbstract;
-use App\Helper\InternalStoreMapper;
 
 class PersonnelCalendarFileService extends PayrollFileServiceAbstract {
     // 00000_10_2024_Begleitpapiere_Datei_AA0.pdf
-    //                                        1               2              3                                              4
+    //                                        1               2              3                                  4
     protected const PATTERN = '/^(?<tenant>\d{5})_(?<month>\d{2})_(?<year>\d{4})_Begleitpapiere_Datei_([A-Z0-9]{2,3})\.pdf$/i';
 
 
