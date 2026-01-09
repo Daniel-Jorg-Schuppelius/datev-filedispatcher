1de303964b8b86ed67cd3dfbdef0c9d10d123e50
DSchuppelius <daniel.schuppelius@gmail.com>
2026-01-09 09:23:29 +0100

    Unfallversicheung

---
+++ src/Services/Payroll/DataProtocolPaymentsContributionInvoiceUVFileService.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Created on   : Wed Feb 26 2025
+ * Author       : Daniel Jörg Schuppelius
+ * Author Uri   : https://schuppelius.org
+ * Filename     : DataProtocolPaymentsContributionInvoiceUVFileService.php
+ * License      : MIT License
+ * License Uri  : https://opensource.org/license/mit
+ */
+
+declare(strict_types=1);
+
+namespace App\Services\Payroll;
+
+use App\Contracts\Abstracts\FileServices\Periodic\PayrollFileServiceAbstract;
+
+class DataProtocolPaymentsContributionInvoiceUVFileService extends PayrollFileServiceAbstract {
+    // 00000_10_2024_DÜ_Prot_Lohnnachweis_Unfallversicherung_AA0.pdf
+    //                                        1               2              3                                                     4
+    protected const PATTERN = '/^(?<tenant>\d{5})_(?<month>\d{2})_(?<year>\d{4})_DÜ_Prot_Lohnnachweis_Unfallversicherung_([A-Z0-9]{2,3})\.pdf$/i';
+
+    protected function getDestinationFilename(): string {
+        $documentType = "DÜ-Protokoll_Lohnnachweis_Unfallversicherung";
+
+        return "{$documentType}.pdf";
+    }
+}
